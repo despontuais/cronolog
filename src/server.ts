1ab86051c9775fe672ff9378c71f92d6dbf3f8bf
Pablo <pablorsantana@hotmail.com>
2024-04-28 23:18:40 -0300

    initial server configuration

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, ErrorRequestHandler } from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import passport from 'passport';
+
+dotenv.config();
+
+const server = express();
+
+server.use(cors());
+server.use(express.static(path.join(__dirname, '../public')));
+server.use(express.urlencoded({extended: true}));
+
+server.use(passport.initialize());
+
+const errorHandler: ErrorRequestHandler = (err, req: Request, res: Response, next) => {
+    err.status ? res.status(err.status) : res.status(400);
+    err.message ? res.json({error: err.message}) : res.json({ error: 'Ocorreu algum erro.' });
+}
+server.use(errorHandler);
+server.listen(process.env.PORT);
