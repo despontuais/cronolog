3431a68e1de9fe3d29b0d0b3c1872e683d2ec61b
Pablo <pablorsantana@hotmail.com>
2024-05-07 00:18:02 -0300

    Creating new routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,6 +4,10 @@ import * as ApiController from '../controllers/apiController';
 const router = Router();
 
 router.get('/ping', ApiController.ping);
+router.get('/user/:id', ApiController.getUser);
+
+router.get('/users', ApiController.listUsers);
+
 
 router.post('/register', ApiController.register);
 
