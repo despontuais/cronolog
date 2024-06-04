23bc253f64ca26d6d84a57fe85dff9a779eb9876
Pablo <pablorsantana@hotmail.com>
2024-06-04 03:59:35 -0300

    using 'privateRoute' middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { login, register, me } from '../controllers/auth';
 import authMiddleware from '../middlewares/auth';
+import { privateRoute } from '../config/passport';
 
 const authRoutes:Router = Router();
 
 authRoutes.post('/register', register);
 authRoutes.post('/login', login);
-authRoutes.get('/me', [authMiddleware], me)
+authRoutes.get('/me', privateRoute, me)
 
 export default authRoutes;
