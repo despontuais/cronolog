f527341703025aad820838d10e7e22dff0e1adc3
Pablo <pablorsantana@hotmail.com>
2024-06-09 22:30:09 -0300

    fixing 'ExtractJwt' and fixing eslint errors

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,37 +1,59 @@
 import passport from "passport";
-import { JWT_SECRET } from "../secrets"
+import { JWT_SECRET } from "../secrets";
 import { ExtractJwt, Strategy as JWTStrategy } from "passport-jwt";
 import { findById } from "../services/UserService";
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 import { User } from "@prisma/client";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import logger from "../libs/logger";
 
-const notAuthorizedJson = {status: 401, message: 'Not Authorized'};
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+const notAuthorizedJson = { status: 401, message: "Not Authorized" };
 
 const options = {
-    // substituí o bearer por um header "Authorization" na request
-    jwtFromRequest: ExtractJwt.fromHeader("Authorization"),
-    secretOrKey: JWT_SECRET
-}
+  // substituí o bearer por um header "Authorization" na request
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: JWT_SECRET,
+};
 
-passport.use(new JWTStrategy(options, async (payload, done) =>{
-    const user = await findById(payload.id);
-    if(!user){
-        done(notAuthorizedJson, false); return;
-    }
-    done(null, user);
-}));
-
-export const generateToken = (data: object) => {
-    return jwt.sign(data, JWT_SECRET, {expiresIn: 90});
-}
-
-export const privateRoute = (req: Request, res: Response, next: NextFunction) =>{
-    const authFunction = passport.authenticate('jwt', (err: any, user: User) => {
-       req.user = user;
-       user ? next() : next(notAuthorizedJson);
+passport.use(
+  new JWTStrategy(options, (payload: User, done) => {
+    (async () => {
+      try {
+        const user = await findById(payload.id);
+        if (!user) {
+          done(notAuthorizedJson, false);
+          return;
+        }
+        done(null, user);
+        return;
+      } catch (err) {
+        done(err, false);
+        return;
+      }
+    })().catch((err: unknown) => {
+      logger.error(err);
     });
-    authFunction(req, res, next);
-}
+  }),
+);
+export const generateToken = (data: object) => {
+  return jwt.sign(data, JWT_SECRET, { expiresIn: "1h" });
+};
+
+export const privateRoute = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  passport.authenticate(
+    "jwt",
+    { session: false },
+    (err: Error | null, user: User) => {
+      req.user = user;
+      err ? next(notAuthorizedJson) : next();
+    },
+  )(req, res, next);
+};
 
 export default passport;
