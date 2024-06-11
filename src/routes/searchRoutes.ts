c59d378fc3bef5723429f84f25fa01f540165d8e
camooz <bertrandrussell78@protonmail.com>
2024-06-11 15:40:03 -0300

    remove unused dependencies

--- src/routes/searchRoutes.ts
+++ src/routes/searchRoutes.ts
@@ -1,8 +1,5 @@
 import express from 'express';
-import authMiddleware from '../middlewares/auth';
 import { search } from '../controllers/searchController';
-import { privateRoute } from '../config/passport';
-
 const router = express.Router();
 
 // Aplica o middleware de autenticação à rota de busca
